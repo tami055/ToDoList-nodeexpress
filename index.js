4a4850bc66dd74f919be5e8353e7e6e1e4cdbf87
tami055 <ktami3116@gmail.com>
2023-05-16 23:24:14 +0300

    jhg

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
+
 import express from 'express'
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
-
-const app=express()
-const port=3000;
-
 const sdk = require('api')('@render-api/v1.0#dnrc1ulf088q9j');
-
-sdk.auth('rnd_bVtXtBNqRyVa5AQupmi5UGtXi5eD');
-sdk.getServices({limit: '20'})
-  .then(({ data }) => console.log(data))
-  .catch(err => console.error(err));
-
-  app.get('/',(req,res)=>{
-    res.send('hello world')
+const port = 3000;
+var app = express();
+app.get('/', (req, res) => { 
+  sdk.getServices({ limit: '20' })
+    .then(({ data }) => res.send(data))
+    .catch(err => console.error(err));
+  // sdk.auth('rnd_b4NkOhh4GRIku6gHd5VBJqXPIvgV');
+  sdk.auth('rnd_bVtXtBNqRyVa5AQupmi5UGtXi5eD');
+  sdk.getServices({ limit: '20' })
+    .then(({ data }) => console.log(data))
+    .catch(err => console.error(err));
 })
-app.listen(port,()=>{
-    console.log(`app listening on http://localhost:${port}`)
+app.listen(port, () => {
+  console.log(`app listening on http://localhost:${port}`)
 })
 console.log("hi")
+
